23bf343c2f988754ff32bd1b8c38d87dd1316283
JuliaOraz <97658697+JuliaOraz@users.noreply.github.com>
2023-03-05 01:26:03 +0200

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -3,8 +3,4 @@ import head from "../image/card-head.png";
 
 export const Container = styled.div`
   padding: 50px 20px;
-  /* text-align: center;
-  background-image: url("${head}");
-  height: 100vh;
-  opacity: 0.05; */
 `;
